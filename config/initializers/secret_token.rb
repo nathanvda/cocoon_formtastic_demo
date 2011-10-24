13f42e5b6964dd072adea79004d1cb274045759e
nathanvda <nathan@dixis.com>
2011-10-24 21:13:21 +0200

    Upgraded to rails 3.1 and formtastic 2.0.2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FormtasticCocoonDemo::Application.config.secret_token = '8a4a3f5ae5a3f489e35fac4b09b4c587d048c955f6c406ec53586090a65bcf1679c9df7c40d99a103af2a49d61f5869a3d0a6727abd3fe04a1ed57806c5016c8'
+FormtasticCocoonDemo::Application.config.secret_token = '0c1c7fa3d37ccad7f2f169c086ba3d6d7e3a7849d2a7fe547a068ae4a5557efe429cd36081354fc33dee5d46bb12230aa56ef4b89d666bf59367382148319540'
